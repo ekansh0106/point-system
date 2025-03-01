6ff0d948f1f47e2c2f44119fc15049da3985a856
ekansh0106 <ekansh0106@gmail.com>
2025-03-01 23:56:41 +0530

    chore: removed dashboard blueprint

--- core/app/blueprints/dashboard/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-dashboard_bp = Blueprint('dashboard', __name__, 
-                        template_folder='templates',
-                        url_prefix='/dashboard')
-
-
-# Import routes after blueprint creation to avoid circular imports
-from . import routes, forms 
